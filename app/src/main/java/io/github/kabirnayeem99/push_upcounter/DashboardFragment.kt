5a6d885e991a5414abfadf72e6bb4d834e7a6be0
Naimul Kabir <39023212+kabirnayeem99@users.noreply.github.com>
2022-12-16 20:02:31 +0600

    build: set up project

---
+++ app/src/main/java/io/github/kabirnayeem99/push_upcounter/DashboardFragment.kt
@@ -0,0 +1,40 @@
+package io.github.kabirnayeem99.push_upcounter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import io.github.kabirnayeem99.push_upcounter.databinding.FragmentDashboardBinding
+
+class DashboardFragment : Fragment() {
+
+    private var _binding: FragmentDashboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//
+//        binding.buttonFirst.setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Push Up Counter
+
+Push Up Counter is an Android application that allows users to track their push up progress over
+time. With this app, users can record their daily push up counts and see their progress in the form
+of a chart.
+
+## Features
+
+- Record daily push up counts
+- View progress in a chart Set push up goals and track progress towards them
+- View historical push up data
